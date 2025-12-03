7eb2e8cfc2b54323beb21302c8017398f69f3f29
jcmoranmartin <jmm0109@alu.medac.es>
2025-12-04 00:30:57 +0100

    Initial Commit

---
+++ MisMangas/MisMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisMangasApp.swift
+//  MisMangas
+//
+//  Created by Juan Carlos on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MisMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
